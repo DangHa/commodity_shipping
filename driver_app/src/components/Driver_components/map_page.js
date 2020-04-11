5239b8a986801e0e25c93ecb7263823153e583ab
dangha <dangha997@gmail.com>
2020-04-11 17:32:51 +0700

    current location button, waiting to fix creating route and dragging pins

--- driver_app/src/components/Driver_components/map_page.js
+++ driver_app/src/components/Driver_components/map_page.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import {StyleSheet, View, Platform, TextInput, Text} from 'react-native';
+import {StyleSheet, View, TextInput, Text, TouchableOpacity} from 'react-native';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 import Geolocation from 'react-native-geolocation-service';
+// import Icon from 'react-native-vector-icons/SimpleLineIcons';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import MapViewDirections from "react-native-maps-directions";
+
 import RetroMapStyles from '../../assets/RetroMapStyles'
-import Icon from 'react-native-vector-icons/SimpleLineIcons';
+
+const GOOGLE_MAP_APIKEY = 'AIzaSyDI3l4n3NL_KbvvLtO8DuSfl4mImgrANoM';
 
 // Get location 
-export const getLocation = () => {
+const getLocation = () => {
   return new Promise(
       (resolve, reject) => {
           Geolocation.getCurrentPosition(
@@ -18,14 +24,20 @@ export const getLocation = () => {
 }
 
 export default class Map extends Component {
-  state = {
-    region: {},
-    start : {},
-    destination: {},
-    startingPredictions: [],
-    startingPointName: "",
-    destinationPredictions: [],
-    destinationName: "",
+
+  constructor(props){
+    super(props);
+    this.state={
+      region: {},
+      start : {},
+      destination: {},
+      startingPredictions: [],
+      startingPointName: "",
+      destinationPredictions: [],
+      destinationName: "",
+      route: {},
+      // origin: {latitude: 21.0250615, longitude: 105.8389873} // test direction
+    }
   }
 
   componentDidMount() {
@@ -51,11 +63,27 @@ export default class Map extends Component {
     this.setState({ region });
   }
 
+  // --- Back to origin location , reset all other location states
+  currentLocation() {
+    this.getInitialState();
+
+    this.setState({
+      start: {}
+    });
+
+    this.setState({
+      description: {}
+    });
+
+    this.setState({
+      route: {}
+    })
+  }
+
   // --- Starting textinput
   async onPressStartingPoint(prediction) {
-    const apiKey = "AIzaSyDI3l4n3NL_KbvvLtO8DuSfl4mImgrANoM"
     const placeid = prediction.place_id
-    const apiUrl = `https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeid}&key=${apiKey}`;
+    const apiUrl = `https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeid}&key=${GOOGLE_MAP_APIKEY}`;
     
     // get coordinate from place id
     var start ={}
@@ -73,7 +101,6 @@ export default class Map extends Component {
       }else {
         start = this.state.region
       }
-      
       
     } catch (err) {
       console.error(err)
@@ -95,7 +122,19 @@ export default class Map extends Component {
       startingPredictions: []
     });
 
-    // from id find coordinate put in start and regoin. create a marker
+    // trigger drawing polyline (route)
+    
+    this.setState({
+      route: {
+        coordinates: [
+          { latitude: this.state.region.latitude, longitude: this.state.region.longitude },
+          { latitude: this.state.start.latitude, longitude: this.state.start.longitude }
+        ]
+      }
+    });
+    
+
+    console.log(this.state.route)
   };
 
   async onChangeStart(start){  
@@ -103,8 +142,7 @@ export default class Map extends Component {
       startingPointName: start
     })
     
-    const apiKey = "AIzaSyDI3l4n3NL_KbvvLtO8DuSfl4mImgrANoM"
-    const apiUrl = `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${apiKey}&input=${start}&location=${this.state.region.latitude}, ${this.state.region.longitude}&radius=20000`;
+    const apiUrl = `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${GOOGLE_MAP_APIKEY}&input=${start}&location=${this.state.region.latitude}, ${this.state.region.longitude}&radius=20000`;
     
     // get prediction from google api
     try {
@@ -120,9 +158,8 @@ export default class Map extends Component {
 
   // --- Destination textinput
   async onPressDestination(prediction) {
-    const apiKey = "AIzaSyDI3l4n3NL_KbvvLtO8DuSfl4mImgrANoM"
     const placeid = prediction.place_id
-    const apiUrl = `https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeid}&key=${apiKey}`;
+    const apiUrl = `https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeid}&key=${GOOGLE_MAP_APIKEY}`;
     
     // get coordinate from place id
     var description = {}
@@ -162,7 +199,19 @@ export default class Map extends Component {
       destinationPredictions: []
     });
 
-    // from id find coordinate put in start and regoin. create a marker
+    // trigger drawing polyline (route)
+    if(this.state.start['latitude']){
+      this.setState({
+        route: {
+          coordinates: [
+            { latitude: this.state.start.latitude, longitude: this.state.start.longitude },
+            { latitude: this.state.destination.latitude, longitude: this.state.destination.longitude }
+          ]
+        }
+      });
+    }
+
+    console.log(this.state.route)
   };
 
   async onChangeDestination(destination){  
@@ -170,8 +219,7 @@ export default class Map extends Component {
       destinationName: destination
     })
     
-    const apiKey = "AIzaSyDI3l4n3NL_KbvvLtO8DuSfl4mImgrANoM"
-    const apiUrl = `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${apiKey}&input=${destination}&location=${this.state.region.latitude}, ${this.state.region.longitude}&radius=20000`;
+    const apiUrl = `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${GOOGLE_MAP_APIKEY}&input=${destination}&location=${this.state.region.latitude}, ${this.state.region.longitude}&radius=20000`;
     
     // get prediction from google api
     try {
@@ -199,6 +247,8 @@ export default class Map extends Component {
       </Text>
     ));
 
+    
+
     return (
       <View style={{ flex: 1 }}>
         {this.state.region['latitude'] ?
@@ -211,9 +261,45 @@ export default class Map extends Component {
               region={this.state.region}
               onRegionChangeComplete={(reg) => this.onMapRegionChange(reg)}
               showsUserLocation={true}>
+
+              {/* --- show the marker --- */}
+              {this.state.start['latitude'] ?
+                <MapView.Marker
+                  draggable
+                  coordinate={{
+                    latitude:  this.state.start.latitude,
+                    longitude: this.state.start.longitude
+                  }}
+                  title={"Starting point"}
+                  description={this.state.startingPointName}
+                />
+              : null}
+              {this.state.destination['latitude'] ?
+                <MapView.Marker
+                  draggable
+                  // onDragEnd={(t, map, coords) => this.setDestination(coords)}
+                  coordinate={{
+                    latitude:  this.state.destination.latitude,
+                    longitude: this.state.destination.longitude
+                  }}
+                  title={"Destination"}
+                  description={this.state.destinationName}
+                />
+              : null}
+
+              {/* --- Route --- */}
+              {this.state.route['coordinates'] ?
+                <MapViewDirections
+                  origin      = {this.state.origin}
+                  destination = {this.state.route.coordinates[1]}
+                  apikey      = {GOOGLE_MAP_APIKEY}
+                  strokeWidth = {3}
+                />
+              : null}
+
             </MapView>
             <View style={styles.inputBox}>
-              <Icon style={styles.imageStyle} size={15} name={'location-pin'} />
+              <Icon style={styles.imageStyle} size={15} name={'location-searching'} />
               <TextInput style={{ flex: 1 }}
                 onChangeText={(start) => this.onChangeStart(start)}
                 underlineColorAndroid='rgba(0,0,0,0)' 
@@ -223,7 +309,7 @@ export default class Map extends Component {
                 value={this.state.startingPointName}/>
             </View>
             <View style={styles.inputBox}>
-              <Icon style={styles.imageStyle} size={15} name={'arrow-right'} />
+              <Icon style={styles.imageStyle} size={15} name={'keyboard-arrow-right'} />
               <TextInput style={{ flex: 1 }}
                   onChangeText={(destination) => this.onChangeDestination(destination)} 
                   underlineColorAndroid='rgba(0,0,0,0)' 
@@ -234,6 +320,17 @@ export default class Map extends Component {
               </View>
               {startingPredictions}
               {destinationPredictions}
+            
+            <View style={styles.bottom}>
+              <TouchableOpacity onPress={this.currentLocation.bind(this)}>
+                <Icon style={styles.imageButton} size={25} name={'my-location'}/>
+              </TouchableOpacity>
+                {/* --- Route --- */}
+                {/* {this.state.route['coordinates'] ?
+                  <Icon style={styles.imageStyle} size={15} name={'my-location'} />
+                : null} */}
+            </View>
+            
           </View>
         : null}
       </View>
@@ -262,6 +359,20 @@ const styles = StyleSheet.create({
   },
   imageStyle: {
     marginLeft: 5
+  },
+  imageButton:{
+    width: 40,
+    height: 40,
+    backgroundColor: '#F5F0F0',
+    borderRadius: 10,
+    paddingHorizontal: 8,
+    paddingVertical: 8,
+    marginLeft: 350
+  },
+  bottom:{
+    flex: 1,
+    justifyContent: 'flex-end',
+    marginBottom: 30
   },
   suggestions: {
     width: 370,
