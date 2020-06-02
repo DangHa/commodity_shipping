84dac60438d640c86aaf7d344fb972678551caca
dangha <dangha997@gmail.com>
2020-06-02 16:00:43 +0700

    sql avoiding bot

--- api/database_SQL/vietnam_routing_query.sql
+++ api/database_SQL/vietnam_routing_query.sql
@@ -2,9 +2,9 @@
 2, Thành phố Thái Bình: 106.3216468 20.4384784
 3, Cảnh Đình Vũ Hải Phòng: 106.766559 20.837813
 4, Sân bay Vân Đồn: 107.419695 21.113701
-5, Hồ Chí Minh City: 106.798081 10.859158
+5, Hồ Chí Minh City: 106.6588245 10.8184631
 
--------------- Cost is just combined of length
+-------------- Cost is just combined of LENGTH
 
 with source_tmp as (SELECT osm_id FROM ways_vertices_pgr 
 		ORDER BY the_geom <-> ST_GeometryFromText('POINT(105.8422401 20.9836114)',4326) 
@@ -21,7 +21,7 @@ SELECT seq, edge, length, ST_AsText(b.the_geom), b.the_geom
 	(SELECT osm_id from source_tmp), (SELECT osm_id from  target_tmp), directed := true, heuristic := 5
 	) a INNER JOIN ways b ON (a.edge = b.gid) ORDER BY seq;
 
-------------------------- Cost is combined of length and penalty ---------------------------
+------------------------- Cost is combined of length and PENALTY ---------------------------
 ALTER TABLE configuration ADD COLUMN penalty FLOAT;
 --
 
@@ -54,6 +54,29 @@ SELECT seq, edge, length, ST_AsText(b.the_geom), b.the_geom
 	(SELECT osm_id from source_tmp), (SELECT osm_id from  target_tmp), directed := true, heuristic := 5
 	) a INNER JOIN ways b ON (a.edge = b.gid) ORDER BY seq;
 
+------------------------- Cost is combined of length, penalty and HIGHEST PRICE ---------------------------
+
+SELECT seq, edge, length, ST_AsText(b.the_geom), b.the_geom
+	FROM pgr_Astar('
+		SELECT gid as id, public."ways".osm_id, source_osm AS source, target_osm AS target, x1, y1, x2, y2, 
+			COALESCE(highest_price, 1) as highest_price, length,
+			CASE
+			   WHEN public."ways".osm_id IN (SELECT osm_id FROM public."BOT_ways") THEN length * penalty * highest_price/100
+			   ELSE length * penalty
+			END 
+			AS cost,
+			CASE
+			   WHEN public."ways".osm_id IN (SELECT osm_id FROM public."BOT_ways") THEN length * penalty * highest_price/100
+			   ELSE length * penalty
+			END 
+			AS reverse_cost
+		FROM ways
+		LEFT JOIN public."BOT_ways" ON public."ways".osm_id = public."BOT_ways".osm_id
+		JOIN configuration USING (tag_id);',
+	(SELECT osm_id from source_tmp), (SELECT osm_id from  target_tmp), directed := true, heuristic := 5
+	) a INNER JOIN ways b ON (a.edge = b.gid) ORDER BY seq;
+
+
 -------------------------- Query for BOT table -------------------------
 
 -- Table of vietnam road system
@@ -72,14 +95,3 @@ DELIMITERS ','
 CSV 
 HEADER;
 
--- Get price
-
-SELECT gid as id, public."ways".osm_id, source_osm AS source, target_osm AS target, x1, y1, x2, y2, 
-		cost as length_cost, reverse_cost, COALESCE(highest_price, 1) as highest_price,
-		CASE
-		   WHEN public."ways".osm_id IN (SELECT osm_id FROM public."BOT_ways") THEN cost*highest_price/100
-		   ELSE cost
-		END 
-		AS cost
-FROM ways
-LEFT JOIN public."BOT_ways" ON public."ways".osm_id = public."BOT_ways".osm_id;
